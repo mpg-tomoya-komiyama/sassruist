1932bd9c48407d9a90527570a4b341ca33a0ac17
mpg-tomoya-komiyama <tomoya.komiyama@medpeer.co.jp>
2019-06-26 22:53:42 +0900

    resolve line

--- src/converter.rs
+++ src/converter.rs
@@ -21,12 +21,20 @@ impl Line {
             None => false,
         }
     }
+
+    fn relove(&mut self, parent: &Line) {
+        if (!self.has_umpersand()) {
+            return;
+        }
+        let parent_selectors = parse_selectors(&parent.text);
+        self.text = resolve_umpersand(&self.text, parent_selectors);
+    }
 }
 
 fn parse_lines(text: &str) -> Vec<Line> {
     let mut lines: Vec<Line> = vec![];
-    let row_lines: Vec<&str> = text.split("\n").collect();
-    for (index, line) in row_lines.iter().enumerate() {
+    let row_lines = text.split("\n");
+    for (index, line) in row_lines.enumerate() {
         lines.push(Line {
             index,
             indent: count_indent(line),
@@ -43,6 +51,30 @@ fn count_indent(line: &str) -> usize {
         }
     }
     return 0;
+}
+
+fn parse_selectors(line: &str) -> Vec<String> {
+    let mut selectors = vec![];
+    let re = Regex::new(r"[^{]*").unwrap();
+    if let Some(cap) = re.captures(line) {
+        let dropped = cap[0].to_string();
+        let splited = dropped.split(',');
+        for s in splited {
+            let trimmed = s.trim();
+            if trimmed != "" {
+                selectors.push(trimmed.to_string());
+            }
+        }
+    }
+    selectors
+}
+
+fn resolve_umpersand(line: &str, parent_selectors: Vec<String>) -> String {
+    let mut selectors: Vec<String> = vec![];
+    for p in parent_selectors {
+        selectors.push(line.replace("&", &p));
+    }
+    selectors.join(", ")
 }
 
 #[cfg(test)]
@@ -66,6 +98,31 @@ mod tests {
         for s in falsy.iter() {
             line.text = s.to_string();
             assert!(!line.has_umpersand());
+        }
+    }
+
+    #[test]
+    fn test_relove() {
+        let mut parent = Line {
+            index: 0,
+            indent: 0,
+            text: "p".to_string(),
+        };
+        let mut line = Line {
+            index: 1,
+            indent: 1,
+            text: "&_a".to_string(),
+        };
+        let data = [
+            ["p", "&_a", "p_a"],
+            ["p q", "&_a", "p q_a"],
+            ["p, q", "&_a", "p_a, q_a"],
+        ];
+        for d in data.iter() {
+            parent.text = d[0].to_string();
+            line.text = d[1].to_string();
+            line.relove(&parent);
+            assert_eq!(line.text, d[2]);
         }
     }
 
@@ -109,5 +166,23 @@ mod tests {
         assert_eq!(count_indent("\t\ta"), 2);
         assert_eq!(count_indent("\t\ta\t"), 2);
         assert_eq!(count_indent(" \ta"), 2);
+    }
+
+    #[test]
+    fn test_parse_selectors() {
+        assert_eq!(parse_selectors(" a "), ["a"]);
+        assert_eq!(parse_selectors("a b"), ["a b"]);
+        assert_eq!(parse_selectors("a, b"), ["a", "b"]);
+        assert_eq!(parse_selectors("a {"), ["a"]);
+        assert_eq!(parse_selectors("a { b {} }"), ["a"]);
+    }
+
+    #[test]
+    fn test_relove_umpersand() {
+        assert_eq!(resolve_umpersand("&_a", vec!("p".to_string())), "p_a");
+        assert_eq!(
+            resolve_umpersand("&_a", vec!("p".to_string(), "q".to_string())),
+            "p_a, q_a"
+        );
     }
 }
