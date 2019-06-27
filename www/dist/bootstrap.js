7833b70155232f3a41bcbdc58e4a5b1336f86e7a
mpg-tomoya-komiyama <tomoya.komiyama@medpeer.co.jp>
2019-06-27 12:45:40 +0900

    [demo] deploy

--- www/dist/bootstrap.js
+++ www/dist/bootstrap.js
@@ -155,7 +155,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/sassruist_bg.wasm":"253441180a43165c262f"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/sassruist_bg.wasm":"48be6e8a9b46bd25ac74"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
