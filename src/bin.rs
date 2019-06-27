b0307928e7f53040a801182dbf47d790c00ce262
mpg-tomoya-komiyama <tomoya.komiyama@medpeer.co.jp>
2019-06-27 16:56:18 +0900

    walk directory

--- src/bin.rs
+++ src/bin.rs
@@ -1,9 +1,13 @@
 #[macro_use]
 extern crate clap;
+extern crate regex;
 extern crate sassruist;
+extern crate walkdir;
 use clap::Arg;
+use regex::Regex;
 use std::fs::File;
 use std::io::Write;
+use walkdir::WalkDir;
 
 pub mod converter;
 pub mod parser;
@@ -17,20 +21,51 @@ fn main() {
         )
         .arg(
             Arg::with_name("overwrite")
-                .help("overwrite original files")
+                .help("overwrite original file(s)")
                 .short("o")
                 .long("overwrite"),
+        )
+        .arg(
+            Arg::with_name("directory")
+                .help("batch execute to the directory")
+                .short("d")
+                .long("directory"),
         );
 
     let matches = app.get_matches();
-    if let Some(o) = matches.value_of("filepath") {
-        if matches.is_present("overwrite") {
-            if let Err(e) = convert_and_write_file(o) {
-                println!("{}", e);
+
+    if matches.is_present("directory") {
+        if let Some(o) = matches.value_of("filepath") {
+            let re = Regex::new(r"\.(?i)(sass|scss)$").unwrap();
+            for entry in WalkDir::new(o).into_iter().filter_map(|e| e.ok()) {
+                let path = format!("{}", entry.path().display());
+                if re.captures(&path).is_some() {
+                    if matches.is_present("overwrite") {
+                        if let Err(e) = convert_and_write_file(&path) {
+                            println!("{}", e);
+                            return;
+                        }
+                    } else {
+                        if let Err(e) = convert_file(&path) {
+                            println!("{}", e);
+                            return;
+                        }
+                    }
+                }
             }
-        } else {
-            if let Err(e) = convert_file(o) {
-                println!("{}", e);
+        }
+    } else {
+        if let Some(path) = matches.value_of("filepath") {
+            if matches.is_present("overwrite") {
+                if let Err(e) = convert_and_write_file(path) {
+                    println!("{}", e);
+                    return;
+                }
+            } else {
+                if let Err(e) = convert_file(path) {
+                    println!("{}", e);
+                    return;
+                }
             }
         }
     }
@@ -40,7 +75,7 @@ pub fn convert_file(path: &str) -> Result<(), String> {
     match parser::parse_file(path) {
         Ok(content) => {
             let text = converter::perform(&content);
-            println!("{}", text);
+            println!("==========\n{}\n==========\n{}", path, text);
             Ok(())
         }
         Err(e) => Err(format!("failed to parse: {}", e)),
