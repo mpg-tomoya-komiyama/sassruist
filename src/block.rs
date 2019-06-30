4f634b2df002b0fdfdbca6fc28948346c8b82072
mpg-tomoya-komiyama <tomoya.komiyama@medpeer.co.jp>
2019-06-30 16:28:54 +0900

    parse block

--- src/block.rs
+++ src/block.rs
@@ -2,7 +2,7 @@ extern crate regex;
 use regex::Regex;
 
 #[derive(Debug, PartialEq)]
-struct Block {
+pub struct Block {
     lines: Vec<String>,
     // head: String,
     // body: String,
@@ -11,25 +11,27 @@ struct Block {
 
 impl Block {
     fn new(lines: Vec<String>) -> Block {
+        let mut children: Vec<Block> = vec!();
+        let head_end = get_head_end(&lines);
+        let block_end = get_block_end(&lines);
+        if head_end < block_end && block_end < lines.len() {
+            children = parse_blocks(&lines[head_end + 1..block_end].to_vec());
+        }
         Block {
             lines,
-            children: vec!(),
+            children,
         }
-    }
-
-    fn parse_children(&self) {
     }
 }
 
-fn parse_blocks(text: &str) -> Vec<Block> {
+pub fn parse_blocks(lines: &Vec<String>) -> Vec<Block> {
     let mut blocks = vec!();
-    let lines: Vec<&str> = text.split("\n").collect();
     let mut index = 0;
 
     while index < lines.len() {
-        let end_index = get_block_end(&lines[index..].to_vec());
+        let (start_index, end_index) = get_block_range(&lines[index..].to_vec());
         let block = Block::new(
-            lines[index..index + end_index + 1]
+            lines[index + start_index..index + end_index + 1]
             .iter().map(|s| s.to_string()).collect());
         blocks.push(block);
         index = index + end_index + 1;
@@ -37,14 +39,14 @@ fn parse_blocks(text: &str) -> Vec<Block> {
     blocks
 }
 
-pub fn get_block_range(lines: Vec<&str>) -> (usize, usize) {
+fn get_block_range(lines: &Vec<String>) -> (usize, usize) {
     let head_end = get_head_end(&lines);
-    let body_end = get_block_end(&lines[head_end + 1..].to_vec());
+    let body_end = get_block_end(&lines[head_end..].to_vec());
     let head_start = get_head_start(&lines, head_end);
     (head_start, body_end)
 }
 
-fn get_head_start(lines: &Vec<&str>, head_end: usize) -> usize {
+fn get_head_start(lines: &Vec<String>, head_end: usize) -> usize {
     let in_head_re = Regex::new(r"(.*,$)|(^( |\t)*$)").unwrap();
     for (index, line) in lines[..head_end].iter().rev().enumerate() {
         if let None = in_head_re.captures(line) {
@@ -54,8 +56,8 @@ fn get_head_start(lines: &Vec<&str>, head_end: usize) -> usize {
     0
 }
 
-fn get_head_end(lines: &Vec<&str>) -> usize {
-    let head_end_re = Regex::new(r"(.*\{$)").unwrap();
+fn get_head_end(lines: &Vec<String>) -> usize {
+    let head_end_re = Regex::new(r"(.*\{.*\}?$)").unwrap();
     for (index, line) in lines.iter().enumerate() {
         if let Some(_) = head_end_re.captures(line.trim()) {
             return index;
@@ -64,7 +66,7 @@ fn get_head_end(lines: &Vec<&str>) -> usize {
     0
 }
 
-fn get_block_end(lines: &Vec<&str>) -> usize {
+fn get_block_end(lines: &Vec<String>) -> usize {
     if lines.len() == 0 { return 0; }
 
     let indent_len = get_indent_text(&lines[0]).len();
@@ -76,7 +78,7 @@ fn get_block_end(lines: &Vec<&str>) -> usize {
     lines.len() - 1
 }
 
-fn get_indent_text(line: &str) -> String {
+fn get_indent_text(line: &String) -> String {
     let pre_re = Regex::new(r"( |\t)*").unwrap();
     if let Some(cap) = pre_re.captures(line) {
         cap[0].to_string()
@@ -90,73 +92,128 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_blocks() {
-        assert_eq!(parse_blocks(&[
+    fn test_block_new_1() {
+        let block = Block::new(vec![
+                   "a {",
+                   " b {",
+                   "  c {",
+                   "  }",
+                   " }",
+                   " b {",
+                   " }",
+                   "}",
+        ].iter().map(|s| s.to_string()).collect());
+        assert_eq!(block.children.len(), 2);
+        assert_eq!(block.children[0].lines, vec![" b {", "  c {", "  }", " }"]);
+        assert_eq!(block.children[0].children.len(), 1);
+        assert_eq!(block.children[0].children[0].lines, vec!["  c {", "  }"]);
+        assert_eq!(block.children[1].lines, vec![" b {", " }"]);
+        assert_eq!(block.children[1].children.len(), 0);
+    }
+
+    #[test]
+    fn test_block_new_2() {
+        let block = Block::new(vec![
+                   "a {",
+                   " b {}",
+                   " c {",
+                   " }",
+                   " b {}",
+                   "}",
+        ].iter().map(|s| s.to_string()).collect());
+        assert_eq!(block.children.len(), 3);
+        assert_eq!(block.children[0].lines, vec![" b {}"]);
+        assert_eq!(block.children[0].children.len(), 0);
+        assert_eq!(block.children[1].lines, vec![" c {", " }"]);
+        assert_eq!(block.children[1].children.len(), 0);
+        assert_eq!(block.children[2].lines, vec![" b {}"]);
+        assert_eq!(block.children[2].children.len(), 0);
+    }
+
+    #[test]
+    fn test_parse_blocks_1() {
+        let blocks = parse_blocks(&vec![
                    "a {",
                    "}",
                    "a {",
                    "}",
-        ].join("\n")), vec!(
+        ].iter().map(|s| s.to_string()).collect());
+        assert_eq!(blocks, vec!(
             Block::new(vec!("a {".to_string(), "}".to_string())),
             Block::new(vec!("a {".to_string(), "}".to_string())),
         ));
     }
 
     #[test]
+    fn test_parse_blocks_2() {
+        let blocks = parse_blocks(&vec![
+                   "a {}",
+                   "b {",
+                   "}",
+        ].iter().map(|s| s.to_string()).collect());
+        assert_eq!(blocks, vec!(
+            Block::new(vec!("a {}".to_string())),
+            Block::new(vec!("b {".to_string(), "}".to_string())),
+        ));
+    }
+
+    #[test]
     fn test_get_head_start() {
         assert_eq!(get_head_start(&vec!(), 0), 0);
-        assert_eq!(get_head_start(&vec!("a {}"), 0), 0);
+        assert_eq!(get_head_start(&vec!("a {}").iter().map(|s| s.to_string()).collect(), 0), 0);
         assert_eq!(get_head_start(&vec!(
                     "}",
                     "",
                     " ",
                     "a {",
-                    "}"), 2), 1);
+                    "}").iter().map(|s| s.to_string()).collect(), 2), 1);
     }
 
     #[test]
     fn test_get_head_end() {
         assert_eq!(get_head_end(&vec!()), 0);
-        assert_eq!(get_head_end(&vec!("a {}")), 0);
+        assert_eq!(get_head_end(&vec!("a {}").iter().map(|s| s.to_string()).collect()), 0);
+        assert_eq!(get_head_end(&vec!("a {}", "b {", "}").iter().map(|s| s.to_string()).collect()), 0);
         assert_eq!(get_head_end(&vec!(
                     "",
                     " ",
                     "a {",
-                    "}")), 2);
+                    "}").iter().map(|s| s.to_string()).collect()), 2);
         assert_eq!(get_head_end(&vec!(
                     "a,",
                     "a, ",
-                    "a {")), 2);
+                    "a {").iter().map(|s| s.to_string()).collect()), 2);
         assert_eq!(get_head_end(&vec!(
                     "a,",
                     "",
                     "a,",
-                    "a {")), 3);
+                    "a {").iter().map(|s| s.to_string()).collect()), 3);
     }
 
     #[test]
     fn test_get_block_end() {
         assert_eq!(get_block_end(&vec!()), 0);
-        assert_eq!(get_block_end(&vec!("a {}")), 0);
+        assert_eq!(get_block_end(&vec!("a {}").iter().map(|s| s.to_string()).collect()), 0);
+        assert_eq!(get_block_end(&vec!("a {}", "b {", "}").iter().map(|s| s.to_string()).collect()), 0);
         assert_eq!(get_block_end(&vec!(
                     "a {",
-                    "}")), 1);
+                    "}").iter().map(|s| s.to_string()).collect()), 1);
         assert_eq!(get_block_end(&vec!(
                     "a {",
                     "}",
                     "a {",
-                    "}")), 1);
+                    "}").iter().map(|s| s.to_string()).collect()), 1);
         assert_eq!(get_block_end(&vec!(
                     "a {",
                     " b {",
                     " }",
-                    "}")), 3);
+                    "}").iter().map(|s| s.to_string()).collect()), 3);
         assert_eq!(get_block_end(&vec!(
                     "a {",
                     " b {",
                     " }",
                     " b {",
                     " }",
-                    "}")), 5);
+                    "}").iter().map(|s| s.to_string()).collect()), 5);
     }
 }
